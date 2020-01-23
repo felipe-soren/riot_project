ef07e339a077d0e6768b11feec96e694ecd4ebe8
unknown <Felipe Novaes>
2020-01-23 20:33:38 -0300

    removendo middleware cors

--- src/service/riot_api_backend.js
+++ src/service/riot_api_backend.js
@@ -5,7 +5,7 @@ export const mainApi = axios.create({
 })
 
 export const riotApi = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://br1.api.riotgames.com/lol/'
+  baseURL: 'https://br1.api.riotgames.com/lol/'
 })
 
 export const API_KEY = "RGAPI-9a06838c-3c12-4410-a7a1-416c01ba69b0";
